e7d345ec01e4eb8f240d4cc4e4d718d9ca9b506e
saahilanande <saahilanande21@gmail.com>
2023-04-29 12:15:56 -0500

    Location search box skeleton added

--- src/Components/SearchBox.tsx
+++ src/Components/SearchBox.tsx
@@ -4,9 +4,12 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  useColorModeValue,
 } from "@chakra-ui/react";
+import { wrap } from "framer-motion";
 import { useState } from "react";
-import { HiDocumentSearch } from "react-icons/hi";
+import { IoSearchSharp } from "react-icons/io5";
+import { MdLocationOn } from "react-icons/md";
 
 interface props {
   onSearchClick: (title: string) => void;
@@ -14,6 +17,7 @@ interface props {
 
 function SearchBox({ onSearchClick }: props) {
   const [titleString, setTitleString] = useState("");
+  const bluemode = useColorModeValue("#3182ce", "#ED8936");
   return (
     <>
       <HStack spacing={2} marginRight={10} marginLeft={10}>
@@ -22,7 +26,7 @@ function SearchBox({ onSearchClick }: props) {
             pointerEvents="none"
             borderLeftRadius={25}
             borderRightRadius={25}
-            children={<HiDocumentSearch color="gray" />}
+            children={<IoSearchSharp color={bluemode} />}
           />
           <Input
             type="text"
@@ -34,11 +38,29 @@ function SearchBox({ onSearchClick }: props) {
             onChange={(val) => setTitleString(val.target.value)}
           />
         </InputGroup>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            borderLeftRadius={25}
+            borderRightRadius={25}
+            children={<MdLocationOn color={bluemode} />}
+          />
+          <Input
+            type="text"
+            id="titleTxt"
+            placeholder="California"
+            borderLeftRadius={25}
+            borderRightRadius={25}
+            value={titleString}
+            onChange={(val) => setTitleString(val.target.value)}
+          />
+        </InputGroup>
         <Button
           borderLeftRadius={25}
           borderRightRadius={25}
           colorScheme="blue"
           onClick={() => onSearchClick(titleString)}
+          width={"20%"}
         >
           Search Jobs
         </Button>
